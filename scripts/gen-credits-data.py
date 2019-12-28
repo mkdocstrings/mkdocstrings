409d729f4e468c55764883ae2f9417be7dcdff4f
Timothée Mazzucotelli <pawamoy@pm.me>
2019-12-28 12:44:35 +0100

    docs: Stop using logo from VLZ in credits

--- scripts/gen-credits-data.py
+++ scripts/gen-credits-data.py
@@ -9,18 +9,8 @@ import toml
 from pip._internal.commands.show import search_packages_info
 
 
-def add_vlz_url(p):
-    try:
-        with open(Path(__file__).parent / "templates" / "vlz" / (p["name"] + ".txt")) as stream:
-            p["vlz-url"] = stream.read().rstrip("\n")
-    except FileNotFoundError:
-        pass
-    return p
-
-
 def clean_info(p):
-    p = add_vlz_url(p)
-    return {k: v for k, v in p.items() if k in ("name", "home-page", "vlz-url")}
+    return {k: v for k, v in p.items() if k in ("name", "home-page")}
 
 
 metadata = toml.load(Path(__file__).parent.parent / "pyproject.toml")["tool"]["poetry"]
@@ -38,9 +28,10 @@ package_info = {p["name"]: clean_info(p) for p in search_packages_info(direct_de
 for dependency in direct_dependencies + indirect_dependencies:
     if dependency not in [_.lower() for _ in package_info.keys()]:
         info = requests.get(f"https://pypi.python.org/pypi/{dependency}/json").json()["info"]
-        package_info[info["name"]] = add_vlz_url(
-            {"name": info["name"], "home-page": info["home_page"] or info["project_url"] or info["package_url"]}
-        )
+        package_info[info["name"]] = {
+            "name": info["name"],
+            "home-page": info["home_page"] or info["project_url"] or info["package_url"],
+        }
 
 lower_package_info = {}
 for package_name, package in package_info.items():
