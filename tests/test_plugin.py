2e85545edae6dea8ed8a9029bf8b789537b482ac
Timothée Mazzucotelli <pawamoy@pm.me>
2020-06-16 21:39:16 +0200

    tests: Add test that builds our own docs

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,8 +1,8 @@
 """Tests for the plugin module."""
+from mkdocs.commands.build import build
+from mkdocs.config.base import load_config
 
 
 def test_plugin():
-    """Very basic test."""
-    from mkdocstrings import plugin
-
-    assert plugin
+    """Build our own documentation."""
+    build(load_config())
