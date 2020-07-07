958bae4dc9364bac9e99ec3eedcada4c831821cf
Timothée Mazzucotelli <pawamoy@pm.me>
2020-07-07 20:20:26 +0200

    style: Fix warnings

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,9 +1,12 @@
+"""Tests for the extension module."""
+
 from markdown import Markdown
 
 from mkdocstrings.extension import MkdocstringsExtension
 
 
 def test_render_html_escaped_sequences():
+    """Assert HTML-escaped sequences are correctly parsed as XML."""
     config = {
         "theme_name": "material",
         "mdx": [],
