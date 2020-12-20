167afd192e0d4b7f4fd9615c75bcea61527836df
Patrick Lannigan <p.lannigan@gmail.com>
2020-12-20 09:06:19 -0500

    feat: Support for reStrcuturedText style

---
+++ docs/snippets/function_annotations_rst.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+
+def my_function(param1: int, param2: Optional[str] = None) -> str:
+    """
+    A short description of this function.
+
+    Complex markup is supported in the main description section.
+
+        I'm a code block!
+
+    :param param1: An integer?
+    :param param2: A string? If you have a long description,
+        you can split it on multiple lines.
+    """
+    return f"{param2}{param1}"
