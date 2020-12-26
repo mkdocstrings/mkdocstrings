5e79de7f116d53813fb96d9c68a7ea2d77947ee9
Oleh Prypin <oleh@pryp.in>
2020-12-26 21:49:37 +0100

    refactor: Theme-agnostic code highlighting, respecting configs
    Infer the highlighter configs from what the current Markdown config is, trying to mimick the two most typical extensions and how they'd highlight a code block in Markdown normally.
    
    This improves theme support, as previously only some assumptions based on each theme's defaults were supported, e.g. the readthedocs theme is assumed to rely only on JS highlighting, when in reality you just need to add a Markdown syntax highlight extension and you can use Pygments.
    
    Another improvement stemming from this is support for disabling syntax highlighting altogether. (`use_pygments: false`). For that I also had to edit the template for function signature, to avoid all newlines in it, because previously inline highlighting relied on processing of the syntax highlighter to get rid of those.

---
+++ tests/test_handlers.py
@@ -0,0 +1,35 @@
+"""Tests for the handlers.base module."""
+
+import pytest
+from markdown import Markdown
+
+from mkdocstrings.handlers.base import Highlighter
+
+
+@pytest.mark.parametrize("extension_name", ["codehilite", "pymdownx.highlight"])
+def test_highlighter_without_pygments(extension_name):
+    configs = {extension_name: {"use_pygments": False, "css_class": "hiiii"}}
+    md = Markdown(extensions=configs, extension_configs=configs)
+    hl = Highlighter(md)
+    assert (
+        hl.highlight("import foo", language="python")
+        == '<pre class="hiiii"><code class="language-python">import foo</code></pre>'
+    )
+    assert (
+        hl.highlight("import foo", language="python", inline=True)
+        == '<code class="highlight language-python">import foo</code>'
+    )
+
+
+@pytest.mark.parametrize("extension_name", [None, "codehilite", "pymdownx.highlight"])
+@pytest.mark.parametrize("inline", [False, True])
+def test_highlighter_basic(extension_name, inline):
+    configs = {}
+    if extension_name:
+        configs[extension_name] = {}
+    md = Markdown(extensions=configs, extension_configs=configs)
+    hl = Highlighter(md)
+
+    actual = hl.highlight("import foo", language="python", inline=inline)
+    assert "import" in actual
+    assert "import foo" not in actual
