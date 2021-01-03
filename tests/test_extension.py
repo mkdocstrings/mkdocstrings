mkdocstrings/mkdocstrings

commit a154f5c4c6ef9abd221e1f89e44847ae2cf25436

fix: Don't let `toc` extension append its permalink twice
PR #203: https://github.com/pawamoy/mkdocstrings/pull/203

src/mkdocstrings/handlers/base.py
@@ -227,11 +227,14 @@ class BaseRenderer(ABC):
             config: Configuration options for `mkdocs` and `mkdocstrings`, read from `mkdocs.yml`. See the source code
                 of [mkdocstrings.plugin.MkdocstringsPlugin.on_config][] to see what's in this dictionary.
         """
-        # Re-instantiate md: see https://github.com/tomchristie/mkautodoc/issues/14
-        md = Markdown(
-            extensions=config["mdx"] + [ShiftHeadingsExtension(), PrefixIdsExtension()],
-            extension_configs=config["mdx_configs"],
-        )
+        extensions = config["mdx"] + [ShiftHeadingsExtension(), PrefixIdsExtension()]
+        configs = dict(config["mdx_configs"])
+        # Prevent a bug that happens due to treeprocessors running on the same fragment both as the inner doc and as
+        # part of the re-integrated doc. Namely, the permalink '¶' would be appended twice. This is the only known
+        # non-idempotent effect of an extension, so specifically prevent it on the inner doc.
+        configs.setdefault("toc", {})["permalink"] = False
+
+        md = Markdown(extensions=extensions, extension_configs=configs)
 
         self.env.filters["convert_markdown"] = functools.partial(do_convert_markdown, md)
 
@@ -409,7 +412,7 @@ class _IdPrependingTreeprocessor(Treeprocessor):
 class PrefixIdsExtension(Extension):
     """Prepend the configured prefix to IDs of all HTML elements."""
 
-    treeprocessor_priority = 4  # Right after 'toc'.
+    treeprocessor_priority = 4  # Right after 'toc' (needed because that extension adds ids to headers).
 
     def extendMarkdown(self, md: Markdown) -> None:  # noqa: N802 (casing: parent method's name)
         """

tests/test_extension.py
@@ -17,7 +17,7 @@ _DEFAULT_CONFIG = {  # noqa: WPS407 (mutable constant)
 def test_render_html_escaped_sequences():
     """Assert HTML-escaped sequences are correctly parsed as XML."""
     config = dict(_DEFAULT_CONFIG)
-    config["mdx"].append(MkdocstringsExtension(config, Handlers(config)))
+    config["mdx"] = [MkdocstringsExtension(config, Handlers(config))]
     md = Markdown(extensions=config["mdx"])
 
     md.convert("::: tests.fixtures.html_escaped_sequences")
@@ -26,7 +26,7 @@ def test_render_html_escaped_sequences():
 def test_multiple_footnotes():
     """Assert footnotes don't get added to subsequent docstrings."""
     config = dict(_DEFAULT_CONFIG, mdx=["footnotes"])
-    config["mdx"].append(MkdocstringsExtension(config, Handlers(config)))
+    config["mdx"] = [MkdocstringsExtension(config, Handlers(config))]
     md = Markdown(extensions=config["mdx"])
 
     output = md.convert(
@@ -52,7 +52,7 @@ def test_multiple_footnotes():
 def test_markdown_heading_level():
     """Assert that Markdown headings' level doesn't exceed heading_level."""
     config = dict(_DEFAULT_CONFIG)
-    config["mdx"].append(MkdocstringsExtension(config, Handlers(config)))
+    config["mdx"] = [MkdocstringsExtension(config, Handlers(config))]
     md = Markdown(extensions=config["mdx"])
 
     output = md.convert("::: tests.fixtures.headings\n    rendering:\n      show_root_heading: true")
@@ -64,9 +64,20 @@ def test_markdown_heading_level():
 def test_reference_inside_autodoc():
     """Assert cross-reference Markdown extension works correctly."""
     config = dict(_DEFAULT_CONFIG)
-    config["mdx"].append(MkdocstringsExtension(config, Handlers(config)))
+    config["mdx"] = [MkdocstringsExtension(config, Handlers(config))]
     md = Markdown(extensions=config["mdx"])
 
     output = md.convert("::: tests.fixtures.cross_reference")
     snippet = 'Link to <span data-mkdocstrings-identifier="something.Else">something.Else</span>.'
     assert snippet in output
+
+
+def test_no_double_toc():
+    """Asserts that the 'toc' extension doesn't apply its modification twice."""
+    config = dict(_DEFAULT_CONFIG)
+    config["mdx_configs"] = {"toc": {"permalink": "@@@"}}
+    config["mdx"] = ["toc", MkdocstringsExtension(config, Handlers(config))]
+    md = Markdown(extensions=config["mdx"], extension_configs=config["mdx_configs"])
+
+    output = md.convert("::: tests.fixtures.headings")
+    assert 3 <= output.count("@@@") < 6

commit 8f6b163b50551da22f65e9b736e042562f77f2d7

fix: Don't mutate the original Markdown config for permalinks
This was a regression from https://github.com/pawamoy/mkdocstrings/pull/203.
This ended up disabling permalinks outright for all docs.

src/mkdocstrings/handlers/base.py
@@ -232,7 +232,10 @@ class BaseRenderer(ABC):
         # Prevent a bug that happens due to treeprocessors running on the same fragment both as the inner doc and as
         # part of the re-integrated doc. Namely, the permalink '¶' would be appended twice. This is the only known
         # non-idempotent effect of an extension, so specifically prevent it on the inner doc.
-        configs.setdefault("toc", {})["permalink"] = False
+        try:
+            configs["toc"] = dict(configs["toc"], permalink=False)
+        except KeyError:
+            pass
 
         md = Markdown(extensions=extensions, extension_configs=configs)
 

tests/test_extension.py
@@ -1,7 +1,9 @@
 """Tests for the extension module."""
+import copy
 from contextlib import contextmanager
 from textwrap import dedent
 
+import pytest
 from markdown import Markdown
 
 from mkdocstrings.extension import MkdocstringsExtension
@@ -25,10 +27,17 @@ def ext_markdown(**kwargs):
         "mkdocstrings": {"default_handler": "python", "custom_templates": None, "watch": [], "handlers": {}},
     }
     config.update(kwargs)
+    original_config = copy.deepcopy(config)
+
     handlers = Handlers(config)
-    config["mdx"].append(MkdocstringsExtension(config, handlers))
+    extension = MkdocstringsExtension(config, handlers)
+    config["mdx"].append(extension)
+    original_config["mdx"].append(extension)
+
     yield Markdown(extensions=config["mdx"], extension_configs=config["mdx_configs"])
     handlers.teardown()
+
+    assert config == original_config  # Inadvertent mutations would propagate to the outer doc!
 
 
 def test_render_html_escaped_sequences():
@@ -86,8 +95,33 @@ def test_reference_inside_autodoc():
     assert snippet in output
 
 
-def test_no_double_toc():
-    """Asserts that the 'toc' extension doesn't apply its modification twice."""
-    with ext_markdown(mdx=["toc"], mdx_configs={"toc": {"permalink": "@@@"}}) as md:
-        output = md.convert("::: tests.fixtures.headings")
-    assert 3 <= output.count("@@@") < 6
+@pytest.mark.parametrize(
+    ("permalink_setting", "expect_permalink"),
+    [
+        ("@@@", "@@@"),
+        (True, "&para;"),
+    ],
+)
+def test_no_double_toc(permalink_setting, expect_permalink):
+    """
+    Assert that the 'toc' extension doesn't apply its modification twice.
+
+    Arguments:
+        permalink_setting: The 'permalink' setting of 'toc' extension.
+        expect_permalink: Text of the permalink to search for in the output.
+    """
+    with ext_markdown(mdx=["toc"], mdx_configs={"toc": {"permalink": permalink_setting}}) as md:
+        output = md.convert(
+            dedent(
+                """
+                # aa
+
+                ::: tests.fixtures.headings
+                    rendering:
+                        show_root_toc_entry: false
+
+                # bb
+                """
+            )
+        )
+    assert output.count(expect_permalink) == 5