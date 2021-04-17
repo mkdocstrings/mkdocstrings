54b12e954baa217b29a45340c06325a37188918b
Oleh Prypin <oleh@pryp.in>
2021-04-17 23:32:39 +0200

    Style changes

--- docs/gen_doc_stubs.py
+++ docs/gen_doc_stubs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 
 from pathlib import Path
-from typing import Union
 
 import mkdocs_gen_files
 
 
-def build_nav(data, indentation=0):
-    lines = []
-    for key in sorted(data):
-        value = data[key]
+def build_nav(data, indentation=""):
+    for key, value in data.items():
         if isinstance(value, dict):
-            lines.append(f"{indentation * ' '}* {key}")
-            lines.extend(build_nav(value, indentation + 4))
+            yield f"{indentation}* {key}\n"
+            yield from build_nav(value, indentation + "    ")
         else:
-            lines.append(f"{indentation * ' '}* [{key}.py]({value})")
-    return lines
+            yield f"{indentation}* [{key}.py]({value})\n"
 
 
 nav_data = {
@@ -26,33 +22,21 @@ nav_data = {
     },
 }
 
-for path in Path("src", "mkdocstrings").glob("**/*.py"):
-    module_path = path.relative_to("src", "mkdocstrings").with_suffix(".md")
-    current_dict: dict = nav_data["mkdocstrings"]  # type: ignore
+for path in sorted(Path("src").glob("**/*.py")):
+    module_path = path.relative_to("src").with_suffix("")
+    doc_path = path.relative_to("src", "mkdocstrings").with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
 
-    for part in module_path.parts:
-        value: Union[str, dict]
+    current_dict = nav_data
+    for part in module_path.parent.parts:
+        current_dict = current_dict.setdefault(part, {})
+    current_dict[module_path.name] = doc_path
 
-        if part.endswith(".md"):
-            key = part[:-3]
-            value = str(module_path)
-        else:
-            key = part
-            value = {}
-
-        if key not in current_dict:
-            current_dict[key] = value
-
-        if isinstance(current_dict[key], dict):
-            current_dict = current_dict[key]
-
-    doc_path = Path("reference", module_path)
-    with mkdocs_gen_files.open(doc_path, "w") as f:
-        ident = ".".join(path.relative_to("src").with_suffix("").parts)
+    with mkdocs_gen_files.open(full_doc_path, "w") as f:
+        ident = ".".join(module_path.parts)
         print("::: " + ident, file=f)
 
-    mkdocs_gen_files.set_edit_path(doc_path, Path("..", path))
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
-with mkdocs_gen_files.open("reference/nav.md", "w") as nav_file:
-    nav_contents = "\n".join(build_nav(nav_data))
-    print(nav_contents, file=nav_file)
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    nav_file.writelines(build_nav(nav_data))
