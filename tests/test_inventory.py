bbd85a92fa70bddfe10a907a4d63b8daf0810cb2
Timothée Mazzucotelli <pawamoy@pm.me>
2021-04-21 20:57:19 +0200

    fix: Fix Sphinx inventory generation
    Issue #265: https://github.com/mkdocstrings/mkdocstrings/issues/265
    PR #267: https://github.com/mkdocstrings/mkdocstrings/pull/267

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,19 +1,20 @@
 """Tests for the inventory module."""
 
+import sys
 from io import BytesIO
 from os.path import join
-from pathlib import Path
 
 import pytest
+from mkdocs.commands.build import build
+from mkdocs.config import load_config
 
 from mkdocstrings.inventory import Inventory, InventoryItem
 
-sphinx_inventory = pytest.importorskip("sphinx.util.inventory", reason="Sphinx is not installed")
-MKDOCSTRINGS_OBJECTS_INV = Path("site/objects.inv")
+sphinx = pytest.importorskip("sphinx.util.inventory", reason="Sphinx is not installed")
 
 
 @pytest.mark.parametrize(
-    "inventory",
+    "our_inv",
     [
         Inventory(),
         Inventory([InventoryItem(name="object_path", domain="py", role="obj", uri="page_url")]),
@@ -21,14 +22,30 @@ MKDOCSTRINGS_OBJECTS_INV = Path("site/objects.inv")
         Inventory([InventoryItem(name="object_path", domain="py", role="obj", uri="page_url#other_anchor")]),
     ],
 )
-def test_sphinx_load_inventory_file(inventory):
+def test_sphinx_load_inventory_file(our_inv):
     """Perform the 'live' inventory load test."""
-    buffer = BytesIO(inventory.format_sphinx())
-    sphinx_inventory.InventoryFile.load(buffer, "", join)
+    buffer = BytesIO(our_inv.format_sphinx())
+    sphinx_inv = sphinx.InventoryFile.load(buffer, "", join)
+
+    sphinx_inv_length = sum(len(sphinx_inv[key]) for key in sphinx_inv)
+    assert sphinx_inv_length == len(our_inv.values())
+
+    for item in our_inv.values():
+        assert item.name in sphinx_inv[f"{item.domain}:{item.role}"]
 
 
-@pytest.mark.skipif(not MKDOCSTRINGS_OBJECTS_INV.exists(), reason="site/objects.inv does not exist")
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="using plugins that require Python 3.7")
 def test_sphinx_load_mkdocstrings_inventory_file():
     """Perform the 'live' inventory load test on mkdocstrings own inventory."""
-    with MKDOCSTRINGS_OBJECTS_INV.open("rb") as fp:
-        sphinx_inventory.InventoryFile.load(fp, "", join)
+    mkdocs_config = load_config()
+    build(mkdocs_config)
+    own_inv = mkdocs_config["plugins"]["mkdocstrings"].handlers.inventory
+
+    with open("site/objects.inv", "rb") as fp:
+        sphinx_inv = sphinx.InventoryFile.load(fp, "", join)
+
+    sphinx_inv_length = sum(len(sphinx_inv[key]) for key in sphinx_inv)
+    assert sphinx_inv_length == len(own_inv.values())
+
+    for item in own_inv.values():
+        assert item.name in sphinx_inv[f"{item.domain}:{item.role}"]
