f9e1a5e8742b5d950fa6407675ee52969ac194d1
Timothée Mazzucotelli <pawamoy@pm.me>
2021-05-01 10:50:05 +0200

    docs: Use literate nav to generate reference sidebar
    Co-authored-by: Oleh Prypin <oleh@pryp.in>
    Issue #179: https://github.com/mkdocstrings/mkdocstrings/issues/179
    Issue #268: https://github.com/mkdocstrings/mkdocstrings/issues/268
    PR #271: https://github.com/mkdocstrings/mkdocstrings/pull/271

--- docs/gen_doc_stubs.py
+++ docs/gen_doc_stubs.py
@@ -4,11 +4,23 @@ from pathlib import Path
 
 import mkdocs_gen_files
 
-for path in Path("src", "mkdocstrings").glob("**/*.py"):
-    doc_path = Path("reference", path.relative_to("src", "mkdocstrings")).with_suffix(".md")
+nav = mkdocs_gen_files.Nav()
 
-    with mkdocs_gen_files.open(doc_path, "w") as f:
-        ident = ".".join(path.relative_to("src").with_suffix("").parts)
+for path in sorted(Path("src").glob("**/*.py")):
+    module_path = path.relative_to("src").with_suffix("")
+    doc_path = path.relative_to("src", "mkdocstrings").with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
+
+    nav[module_path.parts] = doc_path
+
+    with mkdocs_gen_files.open(full_doc_path, "w") as f:
+        ident = ".".join(module_path.parts)
         print("::: " + ident, file=f)
 
-    mkdocs_gen_files.set_edit_path(doc_path, Path("..", path))
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
+
+nav["mkdocs_autorefs", "references"] = "autorefs/references.md"
+nav["mkdocs_autorefs", "plugin"] = "autorefs/plugin.md"
+
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
