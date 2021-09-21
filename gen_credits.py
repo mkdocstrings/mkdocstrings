8bc55029d9729f555520b7613983ff262186796d
Timothée Mazzucotelli <pawamoy@pm.me>
2021-09-21 13:45:03 +0200

    Deployed b05569c with MkDocs version: 1.2.2

--- gen_credits.py
+++ gen_credits.py
@@ -1,13 +1,15 @@
+"""Generate the credits page."""
+
 import functools
+import re
 from itertools import chain
 from pathlib import Path
+from urllib.request import urlopen
 
-import httpx
 import mkdocs_gen_files
 import toml
 from jinja2 import StrictUndefined
 from jinja2.sandbox import SandboxedEnvironment
-from pip._internal.commands.show import search_packages_info  # noqa: WPS436 (no other way?)
 
 
 def get_credits_data() -> dict:
@@ -17,33 +19,26 @@ def get_credits_data() -> dict:
         Data required to render the credits template.
     """
     project_dir = Path(__file__).parent.parent
-    metadata = toml.load(project_dir / "pyproject.toml")["tool"]["poetry"]
-    lock_data = toml.load(project_dir / "poetry.lock")
+    metadata = toml.load(project_dir / "pyproject.toml")["project"]
+    metadata_pdm = toml.load(project_dir / "pyproject.toml")["tool"]["pdm"]
+    lock_data = toml.load(project_dir / "pdm.lock")
     project_name = metadata["name"]
 
-    poetry_dependencies = chain(metadata["dependencies"].keys(), metadata["dev-dependencies"].keys())
-    direct_dependencies = {dep.lower() for dep in poetry_dependencies}
-    direct_dependencies.remove("python")
+    all_dependencies = chain(
+        metadata.get("dependencies", []),
+        chain(*metadata.get("optional-dependencies", {}).values()),
+        chain(*metadata_pdm.get("dev-dependencies", {}).values()),
+    )
+    direct_dependencies = {re.sub(r"[^\w-].*$", "", dep) for dep in all_dependencies}
+    direct_dependencies = {dep.lower() for dep in direct_dependencies}
     indirect_dependencies = {pkg["name"].lower() for pkg in lock_data["package"]}
     indirect_dependencies -= direct_dependencies
-    dependencies = direct_dependencies | indirect_dependencies
-
-    packages = {}
-    for pkg in search_packages_info(dependencies):
-        pkg = {_: pkg[_] for _ in ("name", "home-page")}
-        packages[pkg["name"].lower()] = pkg
-
-    # all packages might not be credited,
-    # like the ones that are now part of the standard library
-    # or the ones that are only used on other operating systems,
-    # and therefore are not installed,
-    # but it's not that important
 
     return {
         "project_name": project_name,
         "direct_dependencies": sorted(direct_dependencies),
         "indirect_dependencies": sorted(indirect_dependencies),
-        "package_info": packages,
+        "more_credits": "http://pawamoy.github.io/credits/",
     }
 
 
@@ -55,13 +50,13 @@ def get_credits():
         The credits page Markdown.
     """
     jinja_env = SandboxedEnvironment(undefined=StrictUndefined)
-    commit = "166758a98d5e544aaa94fda698128e00733497f4"
+    commit = "c78c29caa345b6ace19494a98b1544253cbaf8c1"
     template_url = f"https://raw.githubusercontent.com/pawamoy/jinja-templates/{commit}/credits.md"
     template_data = get_credits_data()
-    template_text = httpx.get(template_url).text
+    template_text = urlopen(template_url).read().decode("utf8")  # noqa: S310
     return jinja_env.from_string(template_text).render(**template_data)
 
 
-with mkdocs_gen_files.open("credits.md", "w") as f:
-    f.write(get_credits())
+with mkdocs_gen_files.open("credits.md", "w") as fd:
+    fd.write(get_credits())
 mkdocs_gen_files.set_edit_path("credits.md", "gen_credits.py")
