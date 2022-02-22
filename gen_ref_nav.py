7410669dea8beecdece888e8e96b4518a77a7dd3
Timothée Mazzucotelli <pawamoy@pm.me>
2022-02-22 23:38:50 +0100

    Deployed 828d51f with MkDocs version: 1.2.3

--- gen_ref_nav.py
+++ gen_ref_nav.py
@@ -12,11 +12,16 @@ for path in sorted(Path("src").glob("**/*.py")):
     full_doc_path = Path("reference", doc_path)
 
     parts = list(module_path.parts)
-    parts[-1] = f"{parts[-1]}.py"
+    if parts[-1] == "__init__":
+        parts = parts[:-1]
+        doc_path = doc_path.with_name("index.md")
+        full_doc_path = full_doc_path.with_name("index.md")
+    elif parts[-1] == "__main__":
+        continue
     nav[parts] = doc_path
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        ident = ".".join(module_path.parts)
+        ident = ".".join(parts)
         print("::: " + ident, file=fd)
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
