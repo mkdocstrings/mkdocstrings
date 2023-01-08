8cf117daeefb4fc522145cc567b40eb4256c0a94
StefanBRas <28559054+StefanBRas@users.noreply.github.com>
2023-01-08 01:55:55 +0100

    feat: Add `enabled` configuration option
    Co-authored-by: Timothée Mazzucotelli <pawamoy@pm.me>
    Issue #478: https://github.com/mkdocstrings/mkdocstrings/issues/478
    PR #504: https://github.com/mkdocstrings/mkdocstrings/pull/504

---
+++ tests/test_plugin.py
@@ -0,0 +1,27 @@
+"""Tests for the mkdocstrings plugin."""
+
+
+from mkdocs.commands.build import build
+from mkdocs.config import load_config
+
+
+def test_disabling_plugin(tmp_path):
+    """Test disabling plugin."""
+    docs_dir = tmp_path / "docs"
+    site_dir = tmp_path / "site"
+    docs_dir.mkdir()
+    site_dir.mkdir()
+    docs_dir.joinpath("index.md").write_text("::: mkdocstrings")
+
+    mkdocs_config = load_config()
+    mkdocs_config["docs_dir"] = str(docs_dir)
+    mkdocs_config["site_dir"] = str(site_dir)
+    mkdocs_config["plugins"]["mkdocstrings"].config["enabled"] = False
+    mkdocs_config["plugins"].run_event("startup", command="build", dirty=False)
+    try:
+        build(mkdocs_config)
+    finally:
+        mkdocs_config["plugins"].run_event("shutdown")
+
+    # make sure the instruction was not processed
+    assert "::: mkdocstrings" in site_dir.joinpath("index.html").read_text()
