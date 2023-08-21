2e10374be258e9713b26f73dd06d0c2520ec07a5
Timothée Mazzucotelli <pawamoy@pm.me>
2023-08-21 16:52:31 +0200

    refactor: Stop accepting sets as return value of `get_anchors` (only tuples), to preserve order

--- src/mkdocstrings/handlers/base.py
+++ src/mkdocstrings/handlers/base.py
@@ -238,7 +238,7 @@ class BaseHandler:
         discovered_extensions = entry_points(group=f"mkdocstrings.{handler}.templates")
         return [extension.load()() for extension in discovered_extensions]
 
-    def get_anchors(self, data: CollectorItem) -> tuple[str, ...] | set[str]:  # noqa: ARG002
+    def get_anchors(self, data: CollectorItem) -> tuple[str, ...]:  # noqa: ARG002
         """Return the possible identifiers (HTML anchors) for a collected item.
 
         Arguments:
