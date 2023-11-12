4dbb6d6a2579d81d65244c0ab1df7e0ee0827fb8
Timothée Mazzucotelli <pawamoy@pm.me>
2023-11-12 01:24:32 +0100

    ci: Ruff auto-fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,12 @@ from typing import TYPE_CHECKING, Any, Iterator
 
 import pytest
 from markdown.core import Markdown
-from mkdocs import config
 from mkdocs.config.defaults import MkDocsConfig
 
 if TYPE_CHECKING:
     from pathlib import Path
+
+    from mkdocs import config
 
     from mkdocstrings.plugin import MkdocstringsPlugin
 
