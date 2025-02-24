a1937077ca6b44c9a932d12dd0a56b925958aa91
Timothée Mazzucotelli <dev@pawamoy.fr>
2025-02-24 20:44:51 +0100

    feat: Support rendering backlinks through handlers
    Handlers must add `backlinks` HTML elements to their templates:
    
    ```html
    <backlinks identifier="some-id" handler="handler-name" />
    ```
    
    mkdocstrings will run a regular expression substitution on each page's HTML, and call corresponding handlers' `render_backlinks` method with backlinks fetched from autorefs (using the specified identifier, and aliases obtained thanks to it through the same handler).
    
    Issue-723: https://github.com/mkdocstrings/mkdocstrings/issues/723
    Issue-mkdocstrings-python-153: https://github.com/mkdocstrings/python/issues/153
    PR-739: https://github.com/mkdocstrings/mkdocstrings/pull/739

--- src/mkdocstrings/handlers/base.py
+++ src/mkdocstrings/handlers/base.py
@@ -21,7 +21,7 @@ from markdown import Markdown
 from markdown.extensions.toc import TocTreeprocessor
 from markupsafe import Markup
 from mkdocs.utils.cache import download_and_cache_url
-from mkdocs_autorefs import AutorefsInlineProcessor
+from mkdocs_autorefs import AutorefsInlineProcessor, BacklinksTreeProcessor
 
 from mkdocstrings._download import download_url_with_gz
 from mkdocstrings.handlers.rendering import (
@@ -44,7 +44,7 @@ if TYPE_CHECKING:
     from collections.abc import Iterable, Iterator, Mapping, Sequence
 
     from markdown import Extension
-    from mkdocs_autorefs import AutorefsHookInterface
+    from mkdocs_autorefs import AutorefsHookInterface, Backlink
 
 log = get_logger(__name__)
 
@@ -323,6 +323,10 @@ class BaseHandler:
         """
         raise NotImplementedError
 
+    def render_backlinks(self, backlinks: Mapping[str, Iterable[Backlink]]) -> str:  # noqa: ARG002
+        """Render backlinks."""
+        return ""
+
     def teardown(self) -> None:
         """Teardown the handler.
 
@@ -412,6 +416,8 @@ class BaseHandler:
         treeprocessors[HeadingShiftingTreeprocessor.name].shift_by = heading_level  # type: ignore[attr-defined]
         treeprocessors[IdPrependingTreeprocessor.name].id_prefix = html_id and html_id + "--"  # type: ignore[attr-defined]
         treeprocessors[ParagraphStrippingTreeprocessor.name].strip = strip_paragraph  # type: ignore[attr-defined]
+        if BacklinksTreeProcessor.name in treeprocessors:
+            treeprocessors[BacklinksTreeProcessor.name].initial_id = html_id  # type: ignore[attr-defined]
 
         if autoref_hook:
             self.md.inlinePatterns[AutorefsInlineProcessor.name].hook = autoref_hook  # type: ignore[attr-defined]
@@ -422,6 +428,8 @@ class BaseHandler:
             treeprocessors[HeadingShiftingTreeprocessor.name].shift_by = 0  # type: ignore[attr-defined]
             treeprocessors[IdPrependingTreeprocessor.name].id_prefix = ""  # type: ignore[attr-defined]
             treeprocessors[ParagraphStrippingTreeprocessor.name].strip = False  # type: ignore[attr-defined]
+            if BacklinksTreeProcessor.name in treeprocessors:
+                treeprocessors[BacklinksTreeProcessor.name].initial_id = None  # type: ignore[attr-defined]
             self.md.inlinePatterns[AutorefsInlineProcessor.name].hook = None  # type: ignore[attr-defined]
             self.md.reset()
             _markdown_conversion_layer -= 1
